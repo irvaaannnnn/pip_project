d8b56b2720d1dd01d79bdcd0df1486dfcda7645f
irvaaannnnn <109333449+irvaaannnnn@users.noreply.github.com>
2025-08-24 15:44:47 +0700

    edit coding data_input.py

--- data_input.py
+++ data_input.py
@@ -1,21 +1,19 @@
 # User menginputkan data
 
-#apapun yg diinputkan pasti isi nya sting
+#STRING=================================
 data = input("Input your data str = ")
-
 #print("Data =", data, "Type =",type(data))
 
-#===============================
+#INTEGER================================
 # jika ingin mengambil int ,maka
 number = int(input("Input your number int ="))
 #print("Data =",number, "Type =",type(number))
 
-
+#FLOAT==================================
 number_float = float(input("Input your number_float float ="))
 #print("Data =",number_float, "Type =",type(number_float))
 
-
-#================================
+#BOOLEAN================================
 #boolean / binner
 biner = bool(int(input("Input your binner = ")))
 print("Hasil adalah..............")
