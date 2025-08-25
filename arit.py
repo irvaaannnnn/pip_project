f87259e8bad9d9fa109cee18f6aef654d498d41c
irvaaannnnn <109333449+irvaaannnnn@users.noreply.github.com>
2025-08-25 10:18:44 +0700

    update arit codes

--- arit.py
+++ arit.py
@@ -1,6 +1,39 @@
-a = input('Masukan angka pertama ')
-b = input('Masukan angka ke dua ')
+#Aritmatika operation
 
-c = (int(a) + int(b))
+#prioritas operasi
+# 1. ()
+# 2. eskponen **
+# 3. perkalian dkk * / % //
+# 4. pertambahan dan pengurangan + -
 
-print('Hasil nya adalah: ' +str(c))
+
+a = 10
+b = 3
+
+#penambahan +
+hasil = a + b
+print(a,"+",b, "=", (hasil))
+
+#pengurangan -
+hasil = a - b
+print(a,"-",b, "=", (hasil))
+
+#perkalian *
+hasil = a * b
+print(a,"*",b, "=", (hasil))
+
+#pembagian /
+hasil = a / b
+print(a,"/",b, "=", (hasil))
+
+#eksponen (pangkat) **
+hasil = a ** b
+print(a,"**",b, "=", (hasil))
+
+#modulus % 10 (dibagi 3 itu berapa sisanya)
+hasil = a % b
+print(a,"%",b, "=", (hasil))
+
+#floor devision // (kebalikan modulus)
+hasil = a // b
+print(a,"//",b, "=", (hasil))
